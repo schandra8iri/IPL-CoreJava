d8161ec5d64e6f2d3017d5bc126ac75257c22b95
sandeep.chandragiri.thbs <sandeep_chandragiri@thbs.com>
2021-08-30 21:35:20 +0530

    first java class

---
+++ src/in/corejava/ipl/IPLTeams.java
@@ -0,0 +1,26 @@
+package in.corejava.ipl;
+
+public class IPLTeams {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("************* IPL 2021 *************");
+		
+		System.out.println("************* IPL TEAM - ROYAL CHALLENGERS BANGALORE *************");
+		System.out.println("CAPTAIN - Virat Koli");
+		System.out.println("WICKET KEEPER - AB de villers");
+		System.out.println("BATSMAN - DevDUtt Padikal");
+		System.out.println("BOWLER - Yuzvendra Chahal");
+		
+		
+		System.out.println("************* IPL TEAM - CHENNAI SUPER KINGS *************");
+		System.out.println("CAPTAIN - MS Dhoni");
+		System.out.println("WICKET KEEPER - MS Dhoni");
+		System.out.println("BATSMAN - Ambati Raidu");
+		System.out.println("BOWLER - Ravindra jadeja");
+
+	
+	}
+
+}
